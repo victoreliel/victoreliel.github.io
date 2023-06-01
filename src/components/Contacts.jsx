5ac013b5ea150f8510a8dfa935e783eed0f5b4a1
victoreliel <victorefm@gmail.com>
2023-06-01 12:49:02 -0300

    feat: animations 'scroll reveal' in experience and contacts components

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,52 +1,77 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  AiFillGithub, AiFillLinkedin, AiFillFacebook, AiFillInstagram, AiOutlineMail,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillFacebook,
+  AiFillInstagram,
+  AiOutlineMail,
   AiFillPhone,
 } from 'react-icons/ai';
 import {SiGooglemaps} from 'react-icons/si';
 import {contactsData} from '../data/contactsData';
 import {socialsData} from '../data/socialsData';
+import scrollReveal from 'scrollreveal';
 import '../styles/Contacts.css';
 
 export default function Contacts() {
   const [contacts] = useState(contactsData);
   const [socials] = useState(socialsData);
 
+  useEffect(() => {
+    scrollReveal().reveal('.scroll-reveal', {
+      duration: 1000,
+      distance: '20px',
+      origin: 'bottom',
+      easing: 'ease-in-out',
+      delay: 100,
+      interval: 200,
+      reset: true,
+    });
+  }, []);
+
   return (
     <div id="contacts" className="contacts">
       <p className="title">Contato</p>
       <div className="basic-contact">
-        <div className="divisions">
+        <div className="divisions scroll-reveal">
           <AiOutlineMail/>
           <p>{contacts.email}</p>
         </div>
-        <div className="divisions">
+        <div className="divisions scroll-reveal">
           <AiFillPhone/>
           <p>{contacts.phone}</p>
         </div>
-        <div className="divisions">
+        <div className="divisions scroll-reveal">
           <SiGooglemaps/>
           <p>{contacts.address}</p>
         </div>
       </div>
       <p className="title">Redes Sociais</p>
       <ul className="socials">
-        <AiFillGithub/>
-        <li>
-          <a href={socials.github} className="social">Github</a>
-        </li>
-        <AiFillLinkedin/>
-        <li>
-          <a href={socials.linkedin} className="social">Linkedin</a>
-        </li>
-        <AiFillFacebook/>
-        <li>
-          <a href={socials.facebook} className="social">Facebook</a>
-        </li>
-        <AiFillInstagram/>
-        <li>
-          <a href={socials.instagram} className="social">Instagram</a>
-        </li>
+        <div className="scroll-reveal row">
+          <AiFillGithub/>
+          <li>
+            <a href={socials.github} className="social">Github</a>
+          </li>
+        </div>
+        <div className="scroll-reveal row">
+          <AiFillLinkedin/>
+          <li>
+            <a href={socials.linkedin} className="social">Linkedin</a>
+          </li>
+        </div>
+        <div className="scroll-reveal row">
+          <AiFillFacebook/>
+          <li>
+            <a href={socials.facebook} className="social">Facebook</a>
+          </li>
+        </div>
+        <div className="scroll-reveal row">
+          <AiFillInstagram/>
+          <li>
+            <a href={socials.instagram} className="social">Instagram</a>
+          </li>
+        </div>
       </ul>
     </div>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {experienceData} from '../data/experienceData';
+import scrollReveal from 'scrollreveal';
 import '../styles/Experience.css';
 
 export default function Experience() {
   const [objects] = useState(experienceData);
 
+  useEffect(() => {
+    scrollReveal().reveal('.experience-item', {
+      distance: '20px',
+      origin: 'bottom',
+      duration: 1000,
+      delay: 100,
+      interval: 200,
+      reset: true,
+    });
+  }, []);
+
   return (
     <div id="experience" className="experience">
-      <p className="title">Experiência Profisional</p>
+      <p className="title">Experiência Profissional</p>
       <div className="experiences">
         {objects.map((object) => (
-          <div key={object.id}>
+          <div key={object.id} className="experience-item">
             <p className="company">{object.company}</p>
             <p className="role">{object.jobtitle}</p>
             <p className="job-years">
