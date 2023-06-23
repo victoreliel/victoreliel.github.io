9f2189a2721b2672f14d5cc74b78b8c5e90a8a05
victoreliel <victorefm@gmail.com>
2023-06-23 11:47:22 -0300

    feat: implementa botão para voltar ao topo das páginas

---
+++ src/components/ButtonTop.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { AiOutlineArrowUp } from 'react-icons/ai';
+
+export default function ButtonTop() {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setShowButton(scrollTop > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className={`${showButton ? 'fixed bottom-4 right-4 z-50' : 'hidden'}`}>
+      <button type="button" onClick={scrollToTop} className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg">
+        <AiOutlineArrowUp />
+      </button>
+    </div>
+  );
+}
